04d4f9acbbaf960c212268295e6cfe555c36b421
Ahmethan Özcan <85957900+AhmethanOzcan@users.noreply.github.com>
2024-08-08 17:54:39 +0300

    Union Find

--- Assets/Scripts/Managers/TileManager.cs
+++ Assets/Scripts/Managers/TileManager.cs
@@ -11,6 +11,7 @@ public class TileManager : Singleton<TileManager>
     public TileType[] _flattenedGrid;
     public Vector3[][] _gridPositions;
     private int _totalTiles;
+    private UnionFind _unionFind;
     Level _level;
 
     protected override void Awake() {
@@ -20,12 +21,13 @@ public class TileManager : Singleton<TileManager>
     public void StartNewLevel(Transform gridTransform)
     {
         this._level             = LevelManager.Instance.GetLevel();
-        _totalTiles          = this._level._rowCount * this._level._columnCount;
+        _totalTiles             = this._level._rowCount * this._level._columnCount;
         this._flattenedGrid     = new TileType[_totalTiles];
-
+        this._unionFind         = new UnionFind(_totalTiles);
         FillGridTransforms(gridTransform);
         CreateTileMatrices();
         FillTiles();
+        PerformUnionFind(0, 0, _level._columnCount);
     }
 
     
@@ -97,10 +99,32 @@ public class TileManager : Singleton<TileManager>
         Tile tileInfo                          = new Tile(new Vector2Int(x, y), type);
         _tileControllers[x][y]                 = tile.GetComponent<TileController>();
         int flatIndex                          = to_index(x,y);
-        _flattenedGrid[to_index(x,y)]          = tileInfo._tileType;
+        _flattenedGrid[flatIndex]              = tileInfo._tileType;
         _tileControllers[x][y].Initialize(tileInfo);
+        
     }
 
+
+    void PerformUnionFind(int xStart, int yStart, int xEnd)
+    {
+        for (int y = yStart; y < _level._rowCount; y++)
+        {
+            for (int x = xStart; x < xEnd; x++)
+            {
+                int index = to_index(x, y);
+                if(_flattenedGrid[index] == TileType.box)
+                    continue;
+                if (x > 0 && _flattenedGrid[index] == _flattenedGrid[to_index(x - 1, y)])
+                {
+                    _unionFind.Union(index, to_index(x - 1, y));
+                }
+                if (y > 0 && _flattenedGrid[index] == _flattenedGrid[to_index(x, y - 1)])
+                {
+                    _unionFind.Union(index, to_index(x, y - 1));
+                }
+            }
+        }
+    }
 
     private int to_index(int x, int y)
     {
@@ -112,5 +136,58 @@ public class TileManager : Singleton<TileManager>
         int x = index%_level._columnCount;
         int y = (index-x)/_level._columnCount;
         return new Vector2Int(x, y);
+    }
+}
+
+public class UnionFind
+{
+    private int[] parent;
+    private int[] size;
+
+    public UnionFind(int n)
+    {
+        parent = new int[n];
+        size = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    public int Find(int index)
+    {
+        if (parent[index] != index)
+        {
+            parent[index] = Find(parent[index]);
+        }
+        return parent[index];
+    }
+
+    public void Union(int index1, int index2)
+    {
+        int root1 = Find(index1);
+        int root2 = Find(index2);
+
+        if (root1 != root2)
+        {
+            if (size[root1] > size[root2])
+            {
+                parent[root2] = root1;
+                size[root1] += size[root2];
+            }
+            else
+            {
+                parent[root1] = root2;
+                size[root2] += size[root1];
+            }
+        }
+    }
+
+    public int GetSize(int index)
+    {
+        int root = Find(index);
+        return size[root];
     }
 }
