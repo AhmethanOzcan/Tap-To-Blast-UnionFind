7a3d739c3bcc93541094e9af5ec52c8523eaf3fe
Ahmethan Özcan <85957900+AhmethanOzcan@users.noreply.github.com>
2024-08-12 19:10:33 +0300

    Corrections Optimizations and More

--- Assets/Scripts/Managers/PoolingManager.cs
+++ Assets/Scripts/Managers/PoolingManager.cs
@@ -7,15 +7,15 @@ public class PoolingManager : Singleton<PoolingManager>
     [SerializeField] int _poolCount = 100;
     [SerializeField] Transform _folder;
     public GameObject _tilePrefab;
+
     private Queue<GameObject> _pooledObjects;
     public bool IsReady { get; private set; }
+
     protected override void Awake() 
     {
         base.Awake();    
     }
 
-
-    // Start is called before the first frame update
     void Start()
     {
         _pooledObjects = new Queue<GameObject>(_poolCount);
@@ -31,15 +31,14 @@ public class PoolingManager : Singleton<PoolingManager>
 
     public GameObject GetPooledObject(Vector3 position)
     {
-        if(_pooledObjects.Count > 0)
-        {
-            GameObject obj = _pooledObjects.Dequeue();
-            obj.transform.position = position;
-            obj.transform.rotation = Quaternion.identity;
-            obj.SetActive(true);
-            return obj;
-        }
-        return null;
+        if(_pooledObjects.Count == 0)
+            return null;
+        
+        GameObject obj = _pooledObjects.Dequeue();
+        obj.transform.SetPositionAndRotation(position, Quaternion.identity);
+        obj.SetActive(true);
+        return obj;
+        
     }
 
     public void ReturnPooledObject(GameObject obj)
